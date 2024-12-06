58fe1082384efee35dcef96e7623044ae80a5b57
Michael Barroco <michael@orbitalize.com>
2024-12-06 22:20:14 +0100

    Select datastore based on COMPOSE_PROFILE

--- build/dev/startup/core_service.sh
+++ build/dev/startup/core_service.sh
@@ -5,11 +5,19 @@
 
 DEBUG_ON=${1:-0}
 
+if [[ "$COMPOSE_PROFILES" == *"with-yugabyte"* ]]; then
+  echo "Using Yugabyte"
+  DATASTORE_CONNECTION="-cockroach_host local-dss-ybdb -cockroach_user yugabyte --cockroach_port 5433"
+else
+  echo "Using CockroachDB"
+  DATASTORE_CONNECTION="-cockroach_host local-dss-crdb"
+fi
+
 if [ "$DEBUG_ON" = "1" ]; then
   echo "Debug Mode: on"
 
   dlv --headless --listen=:4000 --api-version=2 --accept-multiclient exec --continue /usr/bin/core-service -- \
-  -cockroach_host local-dss-crdb \
+  ${DATASTORE_CONNECTION} \
   -public_key_files /var/test-certs/auth2.pem \
   -log_format console \
   -dump_requests \
@@ -21,7 +29,7 @@ else
   echo "Debug Mode: off"
 
   /usr/bin/core-service \
-  -cockroach_host local-dss-crdb \
+  ${DATASTORE_CONNECTION} \
   -public_key_files /var/test-certs/auth2.pem \
   -log_format console \
   -dump_requests \
