228858b62745024311776386cc740c0991f2a80e
Michael Barroco <michael@orbitalize.com>
2025-01-07 18:37:45 +0100

    shell lint

--- build/dev/startup/core_service.sh
+++ build/dev/startup/core_service.sh
@@ -5,7 +5,8 @@
 
 DEBUG_ON=${1:-0}
 
-if [[ "$COMPOSE_PROFILES" == *"with-yugabyte"* ]]; then
+# POSIX compliant test to check if with-yugabyte profile is enabled.
+if [ "${COMPOSE_PROFILES#*"with-yugabyte"}" != "${COMPOSE_PROFILES}" ]; then
   echo "Using Yugabyte"
   DATASTORE_CONNECTION="-cockroach_host local-dss-ybdb -cockroach_user yugabyte --cockroach_port 5433"
 else
@@ -16,8 +17,9 @@ fi
 if [ "$DEBUG_ON" = "1" ]; then
   echo "Debug Mode: on"
 
-  dlv --headless --listen=:4000 --api-version=2 --accept-multiclient exec --continue /usr/bin/core-service -- \
-  ${DATASTORE_CONNECTION} \
+  # Linter is disabled to properly unwrap $DATASTORE_CONNECTION.
+  # shellcheck disable=SC2086
+  dlv --headless --listen=:4000 --api-version=2 --accept-multiclient exec --continue /usr/bin/core-service -- ${DATASTORE_CONNECTION} \
   -public_key_files /var/test-certs/auth2.pem \
   -log_format console \
   -dump_requests \
@@ -28,8 +30,9 @@ if [ "$DEBUG_ON" = "1" ]; then
 else
   echo "Debug Mode: off"
 
-  /usr/bin/core-service \
-  ${DATASTORE_CONNECTION} \
+  # Linter is disabled to properly unwrap $DATASTORE_CONNECTION.
+  # shellcheck disable=SC2086
+  /usr/bin/core-service ${DATASTORE_CONNECTION} \
   -public_key_files /var/test-certs/auth2.pem \
   -log_format console \
   -dump_requests \
